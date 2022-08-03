56f144cd4faf5b4e22c73359dbcff40182387262
Arrhire <nksavchenko96@gmail.com>
2022-08-03 14:20:28 +0300

    fixed Serializer class issue from prev commit, testing profile creation

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -9,17 +9,17 @@ class HelloSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=10)
 
 
-class UserProfileSerializer(serializer.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     """Serializers a user profile object"""
-        class Meta:
-            model = models.UserProfile
-            fields = ('id', 'email', 'name', 'password')
-            extra_kwargs = {
-                'password': {
-                    'write_only': True,
-                    'style': {'input_type': 'password'}
-                }
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
             }
+        }
 
     def create(self, validated_data):
         """Create and return a new User"""
